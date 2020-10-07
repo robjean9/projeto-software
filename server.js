953cd0d603c388676512e52d7b4c7fc0dcef27cf
Robson Jean Penteado <robjean9@hotmail.com>
2020-10-07 11:29:07 -0300

    change start on package.json

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/Jogo'));
 
 app.get('/*', function(req,res) {
     
